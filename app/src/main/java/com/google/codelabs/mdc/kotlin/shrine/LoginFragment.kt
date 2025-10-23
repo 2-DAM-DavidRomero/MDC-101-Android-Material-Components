2f253e8ee6a991ac8f4ba98caf057226a8027308
Práctica 1 <davidromeroforcada@gmail.com>
2025-10-23 16:56:47 +0200

    MDC-101

--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
@@ -1,11 +1,15 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.shrine.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 /**
  * Fragment representing the login screen for Shrine.
@@ -13,14 +17,39 @@ import com.example.shrine.R
 class LoginFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-
-        // Snippet from "Navigate to the next Fragment" section goes here.
-
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment.
         val view = inflater.inflate(R.layout.shr_login_fragment, container, false)
+
+        val nextButton: Button = view.findViewById(R.id.next_button)
+        val passwordEditText: TextInputEditText = view.findViewById(R.id.password_edit_text)
+        val passwordTextInput: TextInputLayout = view.findViewById(R.id.password_text_input)
+
+        // Set an error if the password is less than 8 characters.
+        nextButton.setOnClickListener({
+            if (!isPasswordValid(passwordEditText.text!!)) {
+                passwordTextInput.error = getString(R.string.shr_error_password)
+            } else {
+                // Clear the error.
+                passwordTextInput.error = null
+                // Navigate to the next Fragment.
+                (activity as NavigationHost).navigateTo(ProductGridFragment(), false)
+            }
+        })
+
+        // Clear the error once more than 8 characters are typed.
+        passwordEditText.setOnKeyListener({ _, _, _ ->
+            if (isPasswordValid(passwordEditText.text!!)) {
+                // Clear the error.
+                passwordTextInput.error = null
+            }
+            false
+        })
+
         return view
     }
 
-    // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+    private fun isPasswordValid(text: Editable?): Boolean {
+        return text != null && text.length >= 8
+    }
 }
